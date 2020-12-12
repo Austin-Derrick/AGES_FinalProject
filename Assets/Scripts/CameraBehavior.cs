aae44d126117a6b32e7c1a9fe73163f36ec017fc
Austin-Derrick <aderrickson@colum.edu>
2020-12-12 14:41:39 -0600

    Added player movement and mouse look

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public static CameraBehavior instance;
+
+    public Rigidbody playerRB;
+
+    public float moveSpeed;
+
+    private Vector2 moveInput;
+    private Vector2 mouseInput;
+
+    public float mouseSensitivity = 1f;
+
+    public Transform viewCam;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 horizontalMovement = transform.forward * moveInput.y;
+
+        Vector3 verticalMovement = transform.right * moveInput.x;
+
+        playerRB.velocity = (horizontalMovement + verticalMovement) * moveSpeed;
+
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * mouseSensitivity);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z), mouseSensitivity);
+
+        viewCam.localRotation = Quaternion.Euler(viewCam.localRotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+    }
+}
