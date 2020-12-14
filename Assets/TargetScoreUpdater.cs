32541c55b0c13b888be0568b9dfb6efeaab1ca18
Austin-Derrick <aderrickson@colum.edu>
2020-12-13 22:50:11 -0600

    Created target score tracking and updating logic

---
+++ Assets/TargetScoreUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TargetScoreUpdater : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "SCORE: 0";
+    }
+
+    public void UpdateText(int score)
+    {
+        scoreText.text = $"SCORE: {score}";
+    }
+}
