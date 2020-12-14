5d8c62c5c557e1adf0ff5b4362351042f6f1544e
Austin-Derrick <aderrickson@colum.edu>
2020-12-13 22:44:54 -0600

    Added TextMeshPro package

---
+++ Assets/TargetScoreTracker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScoreTracker : MonoBehaviour
+{
+    int score = 0;
+    [SerializeField]
+    TargetScoreUpdater scoreUpdater;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    public void UpdatePoints(int Value)
+    {
+        score += Value;
+        scoreUpdater.UpdateText(score);
+    }
+}
