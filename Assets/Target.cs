075d09f59ea066f25a874b8b079633746865bb58
Austin-Derrick <aderrickson@colum.edu>
2020-12-13 22:49:51 -0600

    Created a target script to attatch to targets in knife throw area

---
+++ Assets/Target.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField]
+    int scoreValue = 0;
+    [SerializeField]
+    TargetScoreTracker scoreTracker;
+    private void OnCollisionEnter(Collision collision)
+    {
+        scoreTracker.UpdatePoints(scoreValue);
+    }
+}
