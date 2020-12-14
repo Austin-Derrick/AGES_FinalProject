33366288871f7e547852bcc7601dc98c9ece418c
Austin-Derrick <aderrickson@colum.edu>
2020-12-13 22:48:43 -0600

    Score tracker script to keep track of ball throw score

---
+++ Assets/ScoreTracker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+    public int score { get; private set; }
+    [SerializeField]
+    ScoreUpdater scoreText;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Milkjug"))
+        {
+            score++;
+            scoreText.UpdateText(score);
+        }
+    }
+
+    void ResetScore()
+    {
+        score = 0;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+}
