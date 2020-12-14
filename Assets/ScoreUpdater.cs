9ce662cca04d6c4b1b528055ad8062dc0cab9e8f
Austin-Derrick <aderrickson@colum.edu>
2020-12-13 22:49:04 -0600

    Score updated to update score board text

---
+++ Assets/ScoreUpdater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ScoreUpdater : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "SCORE: 0";
+    }
+
+    public void UpdateText(int score)
+    {
+        scoreText.text = $"SCORE: {score}";
+    }
+}
